21da55850463905b6fd563569f1db4901e20aef3
朱骏杰 <ories.zhu@mochain.vip>
2020-09-05 23:51:22 +0800

    借鉴 mdn 之后我们去掉新的语法,用 es3 语法实现

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-function bind(asThis, ...args) {
+var slice = Array.prototype.slice;
+function bind(asThis) {
   // this 就是函数
-  const fn = this //fn1
-  return function(...args2){
-    return fn.call(asThis, ...args,...args2) // fn1.call({ name: 'ories' })
+  var args = slice.call(arguments,1)
+  var fn = this //fn1
+  if(typeof fn !=='function'){
+    throw new Error("bind 必须调用在函数身上")
+  }
+  return function(){
+    var args2= slice.call(arguments,0)
+    return fn.apply(asThis, args.concat(args2)) // fn1.call({ name: 'ories' })
   };
 }
 
